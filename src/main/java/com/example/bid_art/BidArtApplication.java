0bfbf38f151aba013e15134c8f02e99b45384217
rimya <yarim89@gmail.com>
2026-04-24 17:52:34 +0900

    feat: Initial Spring Boot project setup with H2

---
+++ src/main/java/com/example/bid_art/BidArtApplication.java
@@ -0,0 +1,13 @@
+package com.example.bid_art;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BidArtApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BidArtApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/bid_art/BidArtApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.bid_art;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BidArtApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
