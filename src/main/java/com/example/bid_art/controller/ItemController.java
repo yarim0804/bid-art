d70b1502f5d2af2b5731a576959e0d2dabdb2859
rimya <leehyerim000@naver.com>
2026-04-29 16:29:31 +0900

    잔디 확인

--- src/main/java/com/example/bid_art/controller/ItemController.java
+++ src/main/java/com/example/bid_art/controller/ItemController.java
@@ -22,7 +22,7 @@ public class ItemController {
 
     @PostMapping("/api/items")
     public ApiResponse<Item> saveItem(@RequestBody Item item) {
-        // 실제로는 Service 계층을 둬야 하지만, 우선 바로 저장해봅니다.
+    
         Item savedItem = itemRepository.save(item);
         return ApiResponse.success(savedItem);
     }
